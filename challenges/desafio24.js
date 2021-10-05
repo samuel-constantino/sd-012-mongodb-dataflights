a909b9b29edaa53c7496cd20793d88d2cd7e3204
Samuel Constantino <samuelicapuidados@gmail.com>
2021-10-05 17:08:40 -0300

    desafio 23 - encontrar voo com litros de combustivel menor ou iqual a 600 e que restrija lista de empresas

---
+++ challenges/desafio24.js
@@ -0,0 +1,19 @@
+db.voos.find(
+    {
+      $and: [
+        { litrosCombustivel: { $lte: 600 } },
+        {
+          $or: [
+            { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+          ],
+        },
+        { litrosCombustivel: { $exists: true } },
+      ],
+    },
+    {
+      _id: 0,
+      vooId: 1,
+      "empresa.nome": 1,
+      litrosCombustivel: 1,
+    },
+  ).limit(1);
